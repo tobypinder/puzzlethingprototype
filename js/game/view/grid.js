32e93da7699a7754fe618eaee696cbde5aa8c52b
Toby Pinder <toby.pinder@smithelectric.com>
2016-06-27 16:37:47 +0100

    Basic fixed grid skeleton.

--- js/game/view/grid.js
+++ js/game/view/grid.js
@@ -5,47 +5,72 @@ var View_Grid = function(){
     this.view = view;
     this.ctx = view.ctx;
   };
-  this.baseX = 2;
-  this.baseY = 2;
-  this.tileWidth = 16;
-  this.tileHeight = 16;
-  this.lineWidth = 2;
+  this.baseX = 3;
+  this.baseY = 3;
+  this.tileWidth = 12;
+  this.tileHeight = 12;
+
+  this.gridWidth = 6;
+  this.gridHeight = 12;
+
+  this.cursorLX = 2;
+  this.cursorRX = 3;
+  this.cursorY  = 6;
+
+  this.lineWidth = 1;
   this.render = function() {
     // TODO: Render the grid.
+    for(var i=0; i<this.gridWidth; i++) {
+      for(var j=0; j<this.gridHeight; j++) {
+        this.renderCell(i, j);
+      }
+    }
 
-
-    this.renderCross(this.baseX + this.lineWidth, this.baseY + this.lineWidth, this.lineWidth * 2, this.lineWidth * 2);
-    //this.renderCross(this.baseX + this.lineWidth + this.tileWidth, this.baseY + this.lineWidth, this.lineWidth * 2, this.lineWidth * 2);
+    this.renderCross(this.cursorLX, this.cursorY);
+    this.renderCross(this.cursorRX, this.cursorY);
   };
 
-  this.renderCross = function(x, y, w, h) {
+  this.renderCell = function(i, j) {
+    this.renderTileBackground(i, j);
+  }
+
+  this.renderTileBackground = function(i, j) {
+    var x = this.baseX + (i * this.tileWidth);
+    var y = this.baseY + (j * this.tileHeight);
+
+    this.ctx.fillStyle = '#f00';
+    this.ctx.fillRect(x, y, this.tileWidth, this.tileHeight)
+  }
+
+  this.renderCross = function(i, j) {
+    var x = this.baseX + (i * this.tileWidth);
+    var y = this.baseY + (j * this.tileHeight);
+
     this.ctx.strokeStyle = '#fff';
     this.ctx.lineWidth = this.lineWidth;
     this.ctx.lineJoin = "round";
     this.ctx.beginPath();
 
     //tl
-    this.ctx.moveTo(x    , y + h)
-    this.ctx.lineTo(x    , y)
-    this.ctx.lineTo(x + w, y)
+    this.ctx.moveTo(x, y + (this.lineWidth * 2))
+    this.ctx.lineTo(x, y)
+    this.ctx.lineTo(x + (this.lineWidth * 2), y)
 
     //tr
-    this.ctx.moveTo(x + this.tileWidth - w, y)
+    this.ctx.moveTo(x + this.tileWidth - (this.lineWidth * 2), y)
     this.ctx.lineTo(x + this.tileWidth, y)
-    this.ctx.lineTo(x + this.tileWidth, y + h)
+    this.ctx.lineTo(x + this.tileWidth, y + (this.lineWidth * 2))
 
     //bl
-    this.ctx.moveTo(x    , y + this.tileHeight - h)
-    this.ctx.lineTo(x    , y + this.tileHeight)
-    this.ctx.lineTo(x + w, y + this.tileHeight)
+    this.ctx.moveTo(x, y + this.tileHeight - (this.lineWidth * 2))
+    this.ctx.lineTo(x, y + this.tileHeight)
+    this.ctx.lineTo(x + (this.lineWidth * 2), y + this.tileHeight)
 
     //br
-    this.ctx.moveTo(x + this.tileWidth - w, y + this.tileHeight)
+    this.ctx.moveTo(x + this.tileWidth - (this.lineWidth * 2), y + this.tileHeight)
     this.ctx.lineTo(x + this.tileWidth, y + this.tileHeight)
-    this.ctx.lineTo(x + this.tileWidth, y - h + this.tileHeight)
-
+    this.ctx.lineTo(x + this.tileWidth, y - (this.lineWidth * 2) + this.tileHeight)
 
     this.ctx.stroke();
-
   };
 }
